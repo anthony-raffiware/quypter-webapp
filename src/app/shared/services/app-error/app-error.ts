25d1d5a02d3290f201d46d748221ea8697714c90
anthony-raffiware <anthony@raffiware.io>
2026-08-18 16:56:10 -0700

    app-error WIP

---
+++ src/app/shared/services/app-error/app-error.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-error',
+  imports: [],
+  templateUrl: './app-error.html',
+  styleUrl: './app-error.scss',
+})
+export class AppError {}
